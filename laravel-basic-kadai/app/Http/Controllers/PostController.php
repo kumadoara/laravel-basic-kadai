5654625dd95b79322bfd4cfe8232cf5b11358ef0
kumadoara <kw2st68@hotmail.com>
2023-11-06 10:44:45 +0900

    Add files via upload

---
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Post;
+
+class PostController extends Controller {
+    public function index() {
+
+        // postテーブルからすべてのデータを取得し、変数$productsに代入する
+        $posts = DB::table('posts')->get();
+
+        // 変数$postsをposts/index.blade.phpファイルに渡す
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show ($id) {
+        // URL'/posts/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postに代入する
+        $post = Post::find($id);
+
+        // 変数$postをposts/show.blade.phpファイルに渡す
+        return view('posts.show', compact('post'));
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        // バリデーションを設定する
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        // フォームの入力値をもとに、postsテーブルにデータを追加する
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        // リダイレクトさせる
+        return redirect("/posts");
+    }
+}
+
